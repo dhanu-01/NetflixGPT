7b91fdfa7b441380b6e06e0a0e517b062af683eb
Dhanunjai K <dhanunjai@jmangroup.com>
2024-04-19 16:28:17 +0530

    fixed Null values issue

--- src/components/pages/Browse.js
+++ src/components/pages/Browse.js
@@ -2,23 +2,30 @@ import Header from './Header'
 import React, { useEffect, useState } from 'react'
 import Preview from './Preview';
 import { options } from '../../utils/functions/constants';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addNowPlayingMovies,movieTrailer } from '../../Redux/movieSlices';
+import MoviesList from './MoviesList';
 
         
 const Browse = () => {
+  const [trailerKey, setTrailerKey] = useState();
+  const [currentMovie, setCurrentMovie] = useState();
   const dispatch = useDispatch();
   const[loading, setLoading] = useState(false);
-  const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+  const nowPlayingMoviesUrl = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
 
   const getNowPlayingMovies = async () => {
     setLoading(true);
-    const response = await fetch(url,options);
+    const response = await fetch(nowPlayingMoviesUrl,options);
     const json = await response.json();
-    dispatch(addNowPlayingMovies(json.results[0]));
-    console.log(json,'json')
+    setCurrentMovie(json.results[0]);
+    const currentMovieId = json.results[0].id
+    const movieTrailerUrl =  `https://api.themoviedb.org/3/movie/${currentMovieId}/videos?language=en-US`
+    const trailerResponse = await fetch(movieTrailerUrl,options);
+    const trailerJson = await trailerResponse.json();
+    setTrailerKey(trailerJson.results[0].key); 
+    dispatch(addNowPlayingMovies(json.results));
     setLoading(false);
-    
   }
 
   useEffect(()=>{
@@ -29,11 +36,15 @@ const Browse = () => {
   return (
     <div>
        {loading ? 
-            <div>Loading...</div> : 
+             <>  
+                <div>Loading...</div>  
+             </>
+             : 
             (
             <>
               <Header/> 
-              <Preview/>
+               {currentMovie && trailerKey && <Preview currentMovie={currentMovie} trailerKey={trailerKey} />}
+              <MoviesList/>
             </>
        )}
       
---
+++ src/components/pages/MoviesList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MovieList from './MovieList'
+
+const MoviesList = () => {
+  return (
+    <div>
+        <MovieList/>
+    </div>
+  )
+}
+
+export default MoviesList
